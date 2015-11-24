2998cd284710a3e9a88e858c9f7bb7219edcef65
Mervyn McCreight <mervyn.mccreight@gmail.com>
2015-11-24 10:32:44 +0100

    do not access unity-api from the non-main thread, since it is forbidden in unity. move all drawing logic to the update function. DecodeQR (runs in another thread) is now just decoding qr codes.

--- Assets/Scripts/TextureScript.cs
+++ Assets/Scripts/TextureScript.cs
@@ -15,24 +15,25 @@ public class TextureScript : MonoBehaviour {
     private bool runThread = true;
 	private QRCodeData qrCodeData;
 
+	private Color32[] pixels = null;
+	private static int CAM_WIDTH = 1024;
+	private static int CAM_HEIGHT = 768;
+
     // Use this for initialization
     void Start () {
         Application.RequestUserAuthorization(UserAuthorization.WebCam);
-        if (Application.HasUserAuthorization(UserAuthorization.WebCam))
-        {
+        if (Application.HasUserAuthorization(UserAuthorization.WebCam)) {
             Debug.Log("access to webcam granted!");
             Debug.Log("#WebCamDevices: " + WebCamTexture.devices.GetLength(0).ToString());
 
-            foreach (WebCamDevice device in WebCamTexture.devices)
-            {
+            foreach (WebCamDevice device in WebCamTexture.devices) {
                 Debug.Log("WebCamDevice: " + device.name);
                 Debug.Log("FrontFacing? " + device.isFrontFacing);
-                if (!device.isFrontFacing)
-                {
+                if (!device.isFrontFacing) {
                     backFacing = device;
                 }
             }
-            webcamTexture = new WebCamTexture(backFacing.name, 1024, 768);
+            webcamTexture = new WebCamTexture(backFacing.name, CAM_WIDTH, CAM_HEIGHT);
    
             GetComponent<Renderer>().material.SetTexture("_MainTex", webcamTexture);
             webcamTexture.Play();
@@ -40,71 +41,72 @@ public class TextureScript : MonoBehaviour {
             qrCodeThread = new Thread(DecodeQR);
             qrCodeThread.Start();
 
-        } else
-        {
+        } else {
             Debug.LogError("No User Authorization for Camera Device.");
         }
     }
 
-    void DecodeQR()
-    {
-        while (runThread)
-        {
+	// Function for just fetching the QR-Codes in the WebCamTexture-Image
+	// It stores the Data in a QrCodeData-Object.
+	// It uses the "pixels" field as the image-source, since we can not 
+	// access the Unity-API from another thread than the main-thread.
+    void DecodeQR() {
+        while (runThread) {
+			if (pixels != null) {
 
-			LuminanceSource lum = new Color32LuminanceSource(webcamTexture.GetPixels32(), webcamTexture.width, webcamTexture.height);
-			HybridBinarizer bin = new HybridBinarizer(lum);
-			BinaryBitmap binBip = new BinaryBitmap(bin);
-			BitMatrix matrix = binBip.BlackMatrix;
-			Detector detector = new Detector(matrix);
+				LuminanceSource lum = new Color32LuminanceSource(pixels, CAM_WIDTH, CAM_HEIGHT);
+				HybridBinarizer bin = new HybridBinarizer(lum);
+				BinaryBitmap binBip = new BinaryBitmap(bin);
+				BitMatrix matrix = binBip.BlackMatrix;
+				Detector detector = new Detector(matrix);
 
-			DetectorResult result = detector.detect();
-            if (result != null)
-            {
-                ResultPoint[] points = result.Points;
+				DetectorResult result = detector.detect();
+	            if (result != null) {
+	                ResultPoint[] points = result.Points;
 
-				if (qrCodeData == null) {
-					qrCodeData = new QRCodeData(points, null);
-				} else {
-					qrCodeData.Update(points);
-				}
-
-				uiText.text = qrCodeData.ToString();
-
-                Vector3 start = new Vector3((points[0].X - webcamTexture.width / 2) / (float)webcamTexture.width * 1.334f * 10 * -1, 0, (points[0].Y - webcamTexture.height / 2) / (float)webcamTexture.height * 10);
-                Vector3 end1 = new Vector3((points[1].X - webcamTexture.width / 2) / (float)webcamTexture.width * 1.334f * 10 * -1, 0, (points[1].Y - webcamTexture.height / 2) / (float)webcamTexture.height * 10);
-                Vector3 end2 = new Vector3((points[2].X - webcamTexture.width / 2) / (float)webcamTexture.width * 1.334f * 10 * -1, 0, (points[2].Y - webcamTexture.height / 2) / (float)webcamTexture.height * 10);
-                Debug.Log(start);
-                Debug.Log(end1);
-                Debug.Log(end2);
-
-                GameObject[] objects = GameObject.FindGameObjectsWithTag("c1");
-
-                objects[0].transform.localPosition = start;
-                objects[1].transform.localPosition = end1;
-                objects[2].transform.localPosition = end2;
-
-                Debug.Log(result.ToString());
-            }
+					if (qrCodeData == null) {
+						qrCodeData = new QRCodeData(points, null);
+					} else {
+						qrCodeData.Update(points);
+					}
+	            }
+			}
         }
     }
 	
 	// Update is called once per frame
 	void Update () {
+		// first draw upon the old data.
+		if (qrCodeData != null) {
+			uiText.text = qrCodeData.ToString();
+			
+			Vector3 start = new Vector3((qrCodeData.GetPoints()[0].X - CAM_WIDTH / 2) / (float)CAM_WIDTH * 1.334f * 10 * -1, 0, (qrCodeData.GetPoints()[0].Y - CAM_HEIGHT / 2) / (float)CAM_HEIGHT * 10);
+			Vector3 end1 = new Vector3((qrCodeData.GetPoints()[1].X - CAM_WIDTH / 2) / (float)CAM_WIDTH * 1.334f * 10 * -1, 0, (qrCodeData.GetPoints()[1].Y - CAM_HEIGHT / 2) / (float)CAM_HEIGHT * 10);
+			Vector3 end2 = new Vector3((qrCodeData.GetPoints()[2].X - CAM_WIDTH / 2) / (float)CAM_WIDTH * 1.334f * 10 * -1, 0, (qrCodeData.GetPoints()[2].Y - CAM_HEIGHT / 2) / (float)CAM_HEIGHT * 10);
+			
+			GameObject[] objects = GameObject.FindGameObjectsWithTag("c1");
+			
+			objects[0].transform.localPosition = start;
+			objects[1].transform.localPosition = end1;
+			objects[2].transform.localPosition = end2;
+		}
+
+		// then fetch new data for new calculations.
+		pixels = webcamTexture.GetPixels32 ();
     }
 
     // Sent to all game objects before the application is quit.
-    void OnApplicationQuit()
-    {
+    void OnApplicationQuit() {
         runThread = false;
     }
 
     // This function is called when the MonoBehaviour will be destroyed.
-    void OnDestroy()
-    {
+    void OnDestroy() {
         qrCodeThread.Abort();
         webcamTexture.Stop();
     }
 
+	// Class for Mapping QR-Code Data to a Unity GameObject.
 	private class QRCodeData {
 		private ResultPoint[] resultPoints;
 		private GameObject model;
@@ -118,8 +120,11 @@ public class TextureScript : MonoBehaviour {
 			this.resultPoints = newResultPoints;
 		}
 
-		public override string ToString ()
-		{
+		public ResultPoint[] GetPoints() {
+			return this.resultPoints;
+		}
+
+		public override string ToString () {
 			string result = "";
 			foreach (ResultPoint resultPoint in this.resultPoints) {
 				result = result + string.Format("({0}, {1})", resultPoint.X, resultPoint.Y) + System.Environment.NewLine;
