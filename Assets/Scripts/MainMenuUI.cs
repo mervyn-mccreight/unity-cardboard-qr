4e5b9ecae348293eb195ba1530c452aaf80ccbc4
Tim Pauls <tim.pauls.hh@gmail.com>
2016-02-12 16:16:39 +0100

    Added API call to display total question number

--- Assets/Scripts/MainMenuUI.cs
+++ Assets/Scripts/MainMenuUI.cs
@@ -1,13 +1,26 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 using System.Collections;
+using Assets.Scripts;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class MainMenuUI : MonoBehaviour {
+public class MainMenuUI : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
+    // TODO: FH Wedel server URL
+    private const string Url = "http://192.168.1.30/cardboard-qr-questionform/api.php?action=get_question_count";
+
+    // Use this for initialization
+	IEnumerator Start () {
+        var www = new WWW(Url);
+
+        // Wait for download to complete
+        yield return www;
+
+        GameObject.Find("QuestionProgressText").GetComponent<Text>().text = string.Format("Fragen: {0}/{1}", GlobalState.UnlockedCoins.Count, www.text);
+        GameObject.Find("CoinProgressText").GetComponent<Text>().text = string.Format("Münzen: {0}/{1}", GlobalState.CollectedCoins.Count, www.text);
+    }
 	
 	// Update is called once per frame
 	void Update () {
