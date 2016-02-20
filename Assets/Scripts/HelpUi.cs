2ae6dc075209894a24f11ec225facd807f46a79a
Mervyn McCreight <mervyn.mccreight@gmail.com>
2016-02-20 13:38:45 +0100

    proof of concept: outsource ui texts

--- Assets/Scripts/HelpUi.cs
+++ Assets/Scripts/HelpUi.cs
@@ -9,6 +9,8 @@ public class HelpUi : MonoBehaviour {
 	void Start () 
 	{
 		GameObject.Find ("HelpText").GetComponent<Text> ().text = StringResources.HelpText;
+		GameObject.Find ("TitleText").GetComponent<Text> ().text = StringResources.HelpSceneHeading;
+		GameObject.Find ("BackButton").GetComponentInChildren<Text>().text = StringResources.BackButtonText;
 	}
 	
 	// Update is called once per frame
--- Assets/Scripts/StringResources.cs
+++ Assets/Scripts/StringResources.cs
@@ -4,6 +4,13 @@ namespace Assets.Scripts
 {
 	public class StringResources 
 	{
+		// help-scene texts
 		public const string HelpText = "In den Gebäuden der Fachhochschule sind verschiedene QR-Codes versteckt.\nEs gibt Fragen- und Münz-QR-Codes.\nFinde die Fragen-QR-Codes und beantworte die Fragen, um die Münz-QR-Codes freizuschalten.\nNachdem eine Frage eine Münze freigeschaltet hat, sammle diese ein.\nFinde und sammle alle Münzen, um einen Preis zu gewinnen.";
+		public const string HelpSceneHeading = "Anleitung";
+
+		// button texts
+		public const string BackButtonText = "Zurück";
+
+
 	}
 }
