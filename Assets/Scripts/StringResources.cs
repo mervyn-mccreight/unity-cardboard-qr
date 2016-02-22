312646f0ee5d59fd11a3cdf19d1b33d21426b835
Tim Pauls <tim.pauls.hh@gmail.com>
2016-02-23 00:22:42 +0100

    Remade toast layout

--- Assets/Scripts/StringResources.cs
+++ Assets/Scripts/StringResources.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections;
-
-namespace Assets.Scripts 
+﻿namespace Assets.Scripts 
 {
 	public class StringResources 
 	{
@@ -9,7 +7,7 @@ namespace Assets.Scripts
 	    public const string QuestionProgressText = "Fragen: {0}/{1}";
 	    public const string CoinProgressText = "Münzen: {0}/{1}";
 	    public const string GoButtonText = "Los!";
-	    public const string HelpButtonText = "Hilfe";
+	    public const string HelpButtonText = "Anleitung";
 
         // help-scene texts
         public const string HelpText = "In den Gebäuden der Fachhochschule sind verschiedene QR-Codes versteckt.\nEs gibt Fragen- und Münz-QR-Codes.\nFinde die Fragen-QR-Codes und beantworte die Fragen, um die Münz-QR-Codes freizuschalten.\nNachdem eine Frage eine Münze freigeschaltet hat, sammle diese ein.\nFinde und sammle alle Münzen, um einen Preis zu gewinnen.";
@@ -17,10 +15,10 @@ namespace Assets.Scripts
 		public const string BackButtonText = "Zurück";
 
         // camera scene texts
-	    public const string WinToastMessage = "Congratulations! Go collect your prize :D";
-	    public const string QuestionAlreadyAnsweredToastMessage = "Already unlocked!";
-	    public const string CoinAlreadyCollectedToastMessage = "Coin is already collected!";
-        public const string TapCoinToCollectToastMessage = "Tap the screen to capture the coin!";
-        public const string AnswerQuestionFirstToastMessage = "Answer the question first!";
+	    public const string WinToastMessage = "Herzlichen Glückwunsch! Du hast alle Münzen gesammelt. Hole dir deinen Preis am Stand ab.";
+	    public const string QuestionAlreadyAnsweredToastMessage = "Du hast diese Frage bereits beantwortet.";
+	    public const string CoinAlreadyCollectedToastMessage = "Du hast diese Münze bereits eingesammelt.";
+        public const string TapCoinToCollectToastMessage = "Berühre den Bildschirm um diese Münze einzusammeln.";
+        public const string AnswerQuestionFirstToastMessage = "Diese Münze ist noch gesperrt. Suche und beantworte zunächst die richtige Frage.";
 	}
 }
