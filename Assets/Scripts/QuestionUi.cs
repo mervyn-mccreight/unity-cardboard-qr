043958de867504b18b9c5441c7243da81bf19d3e
Tim Pauls <tim.pauls.hh@gmail.com>
2016-03-18 14:32:15 +0100

    Shuffle order of answers

--- Assets/Scripts/QuestionUi.cs
+++ Assets/Scripts/QuestionUi.cs
@@ -1,6 +1,5 @@
 ﻿using System.Collections;
 using System.Linq;
-using System.Runtime.CompilerServices;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
@@ -13,6 +12,7 @@ namespace Assets.Scripts
 
         public AudioSource AudioCorrect;
         public AudioSource AudioFail;
+        private string _correctAnswer;
 
         protected virtual void Start()
         {
@@ -22,12 +22,18 @@ namespace Assets.Scripts
                 GlobalState.Instance.AllQuestions.questions.First(x => x.id == GlobalState.Instance.CurrentQuestion)
                     .ToQuestion();
 
+            // save value of correct answer so we can compare to it later
+            _correctAnswer = _question.Answers[_question.CorrectAnswer];
+
+            // shuffle answers
+            var shuffledAnswers = _question.Answers.OrderBy(x => Random.value * 4).ToArray();
+
             // init UI
             GameObject.Find("QuestionText").GetComponent<Text>().text = _question.Text;
-            GameObject.Find("Answer1Button").GetComponentInChildren<Text>().text = _question.Answers[0];
-            GameObject.Find("Answer2Button").GetComponentInChildren<Text>().text = _question.Answers[1];
-            GameObject.Find("Answer3Button").GetComponentInChildren<Text>().text = _question.Answers[2];
-            GameObject.Find("Answer4Button").GetComponentInChildren<Text>().text = _question.Answers[3];
+            GameObject.Find("Answer1Button").GetComponentInChildren<Text>().text = shuffledAnswers[0];
+            GameObject.Find("Answer2Button").GetComponentInChildren<Text>().text = shuffledAnswers[1];
+            GameObject.Find("Answer3Button").GetComponentInChildren<Text>().text = shuffledAnswers[2];
+            GameObject.Find("Answer4Button").GetComponentInChildren<Text>().text = shuffledAnswers[3];
         }
 
         protected virtual void Update()
@@ -43,7 +49,7 @@ namespace Assets.Scripts
         {
             var button = GameObject.Find(string.Format("Answer{0}Button", answerIndex + 1)).GetComponent<Button>();
             var colors = button.colors;
-            if (_question != null && _question.CorrectAnswer == answerIndex)
+            if (button.GetComponentInChildren<Text>().text.Equals(_correctAnswer))
             {
                 GlobalState.Instance.UnlockCoin();
                 AudioCorrect.Play();
